d5923699d75168912de59c3b6d9eec0a661ef98a
IraSmalko <audir8audir8>
2019-06-10 15:22:40 +0300

    Find out the height of CalendarCarousel

--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel, EventList, WeekdayFormat;
+import 'package:suit_up/models/category.dart';
+import 'package:suit_up/repository/repository.dart';
+
+import 'items_list.dart';
 
 class CalendarPage extends StatefulWidget {
   CalendarPage();
@@ -11,7 +15,8 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  _CalendarPageState();
+  ScrollController _scrollController;
+  static final List<Category> _category = Repository.instance.categories;
 
   DateTime _currentDate = DateTime.now();
 
@@ -33,43 +38,73 @@ class _CalendarPageState extends State<CalendarPage> {
   );
 
   @override
+  void initState() {
+    _scrollController = new ScrollController();
+    _scrollController.addListener(() => setState(() {}));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    CalendarCarousel calendar = buildCalendar();
+    print("WWW ${calendar}");
+
     return Scaffold(
-      primary: true,
-      backgroundColor: Colors.white,
-      body: CalendarCarousel(
-        selectedDateTime: _currentDate,
-        daysHaveCircularBorder: true,
-        markedDatesMap: _markedDateMap,
-        onDayPressed: (DateTime date, List list) {
-          this.setState(() => _currentDate = date);
-        },
-        todayTextStyle: TextStyle(color: Colors.black),
-        todayBorderColor: Colors.black,
-        todayButtonColor: Colors.transparent,
-        thisMonthDayBorderColor: Colors.transparent,
-        selectedDayButtonColor: Colors.black87,
-        selectedDayBorderColor: Colors.transparent,
-        weekDayFormat: WeekdayFormat.short,
-        weekendTextStyle: TextStyle(color: Colors.black),
-        weekdayTextStyle: TextStyle(
-          color: Colors.black,
-          fontStyle: FontStyle.italic,
-        ),
-        headerTextStyle: TextStyle(
-          fontSize: 20,
-          color: Colors.black,
-          fontStyle: FontStyle.italic,
-          fontWeight: FontWeight.w600,
-        ),
-        leftButtonIcon: Icon(
-          Icons.keyboard_arrow_left,
-          color: Colors.black,
-        ),
-        rightButtonIcon: Icon(
-          Icons.keyboard_arrow_right,
-          color: Colors.black,
-        ),
+      appBar: null,
+      body: Stack(
+        children: <Widget>[
+          NestedScrollView(
+            scrollDirection: Axis.vertical,
+            controller: _scrollController,
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverList(delegate: SliverChildBuilderDelegate((ctx, int) {
+                  return int == 0 ? calendar : null;
+                }))
+              ];
+            },
+            body: ItemsList(Repository.instance.dress),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildCalendar() {
+    return CalendarCarousel(
+      height: 320,
+      selectedDateTime: _currentDate,
+      daysHaveCircularBorder: true,
+      markedDatesMap: _markedDateMap,
+      onDayPressed: (DateTime date, List list) {
+        this.setState(() => _currentDate = date);
+      },
+      todayTextStyle: TextStyle(color: Colors.black),
+      todayBorderColor: Colors.black,
+      todayButtonColor: Colors.transparent,
+      thisMonthDayBorderColor: Colors.transparent,
+      selectedDayButtonColor: Colors.black87,
+      selectedDayBorderColor: Colors.transparent,
+      weekDayFormat: WeekdayFormat.short,
+      weekendTextStyle: TextStyle(color: Colors.black),
+      weekdayTextStyle: TextStyle(
+        color: Colors.black,
+        fontStyle: FontStyle.italic,
+      ),
+      headerTextStyle: TextStyle(
+        fontSize: 20,
+        color: Colors.black,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.w600,
+      ),
+      headerMargin: EdgeInsets.symmetric(vertical: 2.0, horizontal: 6.0),
+      leftButtonIcon: Icon(
+        Icons.keyboard_arrow_left,
+        color: Colors.black,
+      ),
+      rightButtonIcon: Icon(
+        Icons.keyboard_arrow_right,
+        color: Colors.black,
       ),
     );
   }
